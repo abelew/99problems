503df3cc4b18c860a210945afe1f4f64dc9d8614
Ashton Trey Belew <abelew@gmail.com>
2015-09-22 14:32:10 -0400

    added a starter for haskell

---
+++ haskell/01.hs
@@ -0,0 +1,53 @@
+-- Get the last element of a list
+--    * (my-last '(a b c d))<br>
+--    (D)<p>
+
+-- The following is from:
+-- learnyouahaskell.com/
+
+double x = x + x
+-- double_two x y = double x + double y
+
+-- bob = "test" -- this is still a function
+-- functions can not begin with capital letters in haskell.
+-- appending lists with ++  [1,2,3] ++ [4,5,6] => [1,2,3,4,5,6]
+-- strings are lists?  "hel" ++ "lo" => "hello"
+-- ['h','e'] ++ ['l','l','o'] => 'hello'
+-- lisp's cons operator is ':'
+-- 'Good' : " kitty!" => "Good kitty!"
+-- 1 : [2,3,4] => [1,2,3,4]
+-- list indexes: [1,2,3] ! 2 => 3   [1,2,3] ! 0 => 1
+-- head == car, tail == cdr, last(reversehead), init(reverse cdr) [1,2,3] => [1,2]
+-- length, null, reverse, take 3 [1,2,3,4] => [1,2,3] (takes first 3)
+-- drop drops the first n elements
+-- minimum maximum sum product
+-- infix operations:  3 `elem` [1,2,3,4] ==  elem 3 [1,2,3,4]
+-- [x * 2 | x <- [1..4]] => [2,4,6,8]
+-- all numbers divisible by 7 from 1..1000:
+-- [x | x <- [1 .. 1000], mod x 7 == 0]
+
+main = do
+  putStrLn "hello"
+  let first_try = mylast mylist
+  print first_try
+  let second_try = mylast' mylist
+  print second_try
+--  first <- mylast mylist
+--  second <- mylast' mylist
+--  putStrLn("The answer is " ++ first ++ " from first, and " ++ second ++ " from second.")
+
+
+mylist = [1,2,3,4,5,6,7,8,9,0]
+
+mylast :: [a] -> [a]
+mylast [] = []
+mylast [a] = [a]
+mylast (a:b) = mylast b
+
+mylast' :: [a] -> [a]
+mylast' [] = []
+mylast' [a] = [a]
+mylast' (a:b) = if null(tail (a:b))
+                then [a]
+                else mylast b
+
---
+++ haskell/README.md
@@ -0,0 +1,21 @@
+# Haskell
+
+In order to play in haskell, I am currently using emacs'
+haskell-mode.  I then do the following steps (so far):
+1.  When I open a new .hs file, do meta-x haskell-interactive-mode.
+2.  I do meta-x run-haskell to open a ghci session.
+3.  In the .hs file I make a block named appropriately 'main'
+4.  When ready, I type :l filename.hs in the ghci session.
+5.  I run main inside the haskell session.
+
+Theoretically C-c C-c (control-c control-c) should load my file
+automagically in the running haskell process.  But I haven't gotten it
+working properly yet, I dunno why.
+
+I am putting a Makefile to compile these scripts, but it looks like
+the only thing one needs to do to compile test.hs is:
+
+```
+ghc -o test test.hs
+```
+
